4576bdd439455244e21af014de746c2f677aa231
panwal <talk2lewamtitus@gmail.com>
2020-02-04 10:15:46 +0000

    first commit

---
+++  app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+db = SQLAlchemy()
+
+
+def create_app():
+    """Construct the core application."""
+    # app = Flask(__name__, instance_relative_config=False)
+    db.init_app(app)
+    app.config.from_object('config.Config')
+
+    with app.app_context():
+        # Imports route
+        from . import routes
+
+        # Create tables for our models
+        db.create_all()
+
+        return app
---
+++  app/routes.py
@@ -0,0 +1,36 @@
+from flask import request, render_template, make_respone
+from datetime import current_app as dt
+from flask import db, pin
+from random import randint
+from flask import Flask
+
+
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = 10**(n-1)-1
+    return randint(range_start, range_end)
+
+@app.route('/api/v1/pin/generate/', methods=['GET'])
+def generate_pin():
+    """generate pin."""
+
+   new_pin = Number(
+        digit=random_with_N_digits(15)
+    )
+    # Adds new User record to database
+    db.session.add(new_pin)
+    # Commits all changes
+    db.session.commit()  
+    return make_response({'pin': new_pin.digit, 'serial': 
+    f'0{new_pin.id}', 'message': 'Pin generated sucesfully!'})  
+
+@app.route('/api/<serial>')
+def validate_pin(pin, serial):
+    """Validate pin"""
+    pin = int(pin)
+    serial = int(serial)
+    db_pin = Pin.query.filter_by(digit=pin, id=serial).first()
+    if db_pin:
+        return make_response({'message': 'valid Pin'})
+    return make_response({'message': 'Pin doest not exists ...!'}), 404
